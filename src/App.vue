e7c6fb32faa48fcc84ac0d2ac21915d1e9fb2055
Myat Mon <ayemyatmon.yn@gmail.com>
2025-03-26 16:17:23 +0000

    change UI

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,14 @@
 </template>
 
 <script>
-import HomePage from './components/HomePage.vue'
+import HomePage from "./components/HomePage.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     HomePage,
-  }
-}
+  },
+};
 </script>
 
 <style>
@@ -19,7 +19,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #ffffff;
   margin: 10px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,18 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/styles/main.css";
 
-
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faTint, faTachometerAlt, faWind, faTrash, faEraser } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faTint,
+  faTachometerAlt,
+  faWind,
+  faTrash,
+  faEraser,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 library.add(faTint, faTachometerAlt, faWind, faTrash, faEraser);
 
-createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
-
-
-
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
